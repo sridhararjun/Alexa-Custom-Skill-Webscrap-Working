fa55e3daa859e53324b074b9f04da66d09a7517a
SM <swadesSNV@gmail.com>
2017-08-24 13:59:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var APP_ID     = '';
 var utils      =require('util');
 // , MTA_KEY    = process.env.MTA_KEY;
 var outputMessage="There are %d shops available at";
+var ua         =require('universal-analytics');
+var tracking   = ua('UA-104879380-1');
+
 var url = function(shop,place){
   return 'http://directory.stokesentinel.co.uk/search/' + place + '/' + shop;
 };
@@ -36,10 +39,11 @@ var getJsonResponse = function(shop,place, callback){
 
 var searchBusinessRequest = function(intent, session, response){
   getJsonResponse(intent.slots.shop.value,intent.slots.place.value, function(data){
-
+var requestedData = ("inputDate: " + new Date() ).toString();
+tracking.event("success", requestedData).send();
     if(data.length>0){
       var voiceText = utils.format(outputMessage,data.length);
-      var cardContent = voiceText+" "intent.slots.place.value+"\n Here are the first five.\n  1)"+data[0]+"\n  2)"+data[1]+"\n  3)"+data[2]+"\n  4)"+data[3]+"\n  5)"+data[4];
+      var cardContent = voiceText+" "+intent.slots.place.value+"\n Here are the first five.\n  1)"+data[0]+"\n  2)"+data[1]+"\n  3)"+data[2]+"\n  4)"+data[3]+"\n  5)"+data[4];
       voiceText    += " "+intent.slots.place.value+" I have sent you the details to your Alexa app  Here are the top five shops  The first is "+data[0]+"  The second is "+data[1]+"  The third is "+data[2]+"  The fourth is "+data[3]+"  The fifth shop is "+data[4];
       
     }
